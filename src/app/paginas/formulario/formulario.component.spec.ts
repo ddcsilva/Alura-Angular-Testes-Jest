029976f353c85609b21a84aa783260d5260debf1
Danilo Silva <danilo.silva@msn.com>
2025-02-23 18:37:07 -0300

    adiciona teste para verificar a adição de um novo livro no FormularioComponent

--- src/app/paginas/formulario/formulario.component.spec.ts
+++ src/app/paginas/formulario/formulario.component.spec.ts
@@ -32,4 +32,41 @@ describe('FormularioComponent', () => {
       classificacao: null,
     });
   });
+
+  it('deveria adicionar um novo livro', () => {
+    // Arrange
+    const novoLivro = {
+      titulo: 'É assim que acaba',
+      autoria: 'Colleen Hoover',
+      imagem:
+        'https://images-na.ssl-images-amazon.com/images/I/51Z4t9bZ7HL._SX331_BO1,204,203,200_.jpg',
+      genero: 'romance',
+      dataLeitura: '2021-09-15',
+      classificacao: 5,
+    };
+
+    const adicionarLivroSpy = jest.spyOn(service, 'adicionarLivro');
+    const routerSpy = jest.spyOn(component['router'], 'navigate');
+
+    // Act
+    component.formulario.setValue(novoLivro);
+    component.adicionarLivro();
+
+    // Assert
+    expect(adicionarLivroSpy).toHaveBeenCalledWith({
+      ...novoLivro,
+      genero: component.generos.find((g) => g.id === novoLivro.genero),
+    });
+
+    expect(component.formulario.value).toEqual({
+      titulo: null,
+      autoria: null,
+      imagem: null,
+      genero: null,
+      dataLeitura: null,
+      classificacao: null,
+    });
+
+    expect(routerSpy).toHaveBeenCalledWith(['lista-livros']);
+  });
 });
